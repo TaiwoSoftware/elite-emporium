cdf8ceb81d134079213edc5321ad839f177c2ab5
TaiwoSoftware <davcodes24@gmail.com>
2024-12-21 23:50:35 +0100

    still fixing them

--- src/app/components/MenCategories/MenCategoriesPattern.jsx
+++ src/app/components/MenCategories/MenCategoriesPattern.jsx
@@ -9,12 +9,12 @@ export default function MenCategoriesPattern({
       <Image src={productImg} alt="product Image" />
       <p className="text-belowGray font-inter font-bold">{nameOfMenProduct}</p>
       <div className="flex gap-4 rounded-lg">
-        <div className="bg-navbarGray p-4">XS</div>
-        <div className="bg-navbarGray p-4">S</div>
-        <div className="bg-navbarGray p-4">M</div>
-        <div className="bg-navbarGray p-4">L</div>
-        <div className="bg-navbarGray p-4">XL</div>
-        <div className="bg-navbarGray p-4">XXL</div>
+        <div className="bg-navbarGray p-2">XS</div>
+        <div className="bg-navbarGray p-2">S</div>
+        <div className="bg-navbarGray p-2">M</div>
+        <div className="bg-navbarGray p-2">L</div>
+        <div className="bg-navbarGray p-2">XL</div>
+        <div className="bg-navbarGray p-2">XXL</div>
       </div>
       <p className="font-bold text-searchBorder">{price}</p>
     </div>
